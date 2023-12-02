2f5a8b2e2678d91536f44f7eb473444b4b42f82a
Emile Frey <24356622+emilefrey@users.noreply.github.com>
2023-12-01 23:32:32 -0600

    day 2 done

--- 02.js
+++ 02.js
@@ -2,12 +2,69 @@ var helpers = require("./helpers");
 
 const part1 = (data) => {
   const lines = helpers.splitByNewLine(data);
-  return 1;
+  let total = 0;
+  lines.forEach((line) => {
+    let possible = true;
+    let [gameId, games] = line.split(": ");
+    gameId = parseInt(gameId.split(" ")[1]);
+    const gamesArray = games.split("; ");
+    gamesArray.forEach((draw) => {
+      const colorDraw = draw.split(", ");
+      colorDraw.forEach((color) => {
+        let [count, colorDrawn] = color.split(" ");
+        count = parseInt(count);
+        if (count > maxLookup[colorDrawn]) {
+          possible = false;
+          // TODO: Break (not necessary, but could speed things up...I think I need a for loop)
+        }
+      });
+    });
+    if (possible) {
+      total += gameId;
+    }
+  });
+  return total;
+};
+
+const maxLookup = {
+  blue: 14,
+  red: 12,
+  green: 13,
 };
 
 const part2 = (data) => {
   const lines = helpers.splitByNewLine(data);
-  return 2;
+  let total = 0;
+  lines.forEach((line) => {
+    let maxBlue = 0;
+    let maxRed = 0;
+    let maxGreen = 0;
+    let games = line.split(": ")[1];
+    const gamesArray = games.split("; ");
+    gamesArray.forEach((draw) => {
+      const colorDraw = draw.split(", ");
+      colorDraw.forEach((color) => {
+        let [count, colorDrawn] = color.split(" ");
+        count = parseInt(count);
+        if (colorDrawn === "red") {
+          maxRed = determineMax(maxRed, count);
+        } else if (colorDrawn === "blue") {
+          maxBlue = determineMax(maxBlue, count);
+        } else if (colorDrawn === "green") {
+          maxGreen = determineMax(maxGreen, count);
+        }
+      });
+    });
+    total += maxBlue * maxRed * maxGreen;
+  });
+  return total;
+};
+
+const determineMax = (currentMax, currentValue) => {
+  if (currentValue > currentMax) {
+    return currentValue;
+  }
+  return currentMax;
 };
 
 module.exports = {
