f01585b72b6f3fb4ffcc9227e79d57d2128d9f66
Emile Frey <24356622+emilefrey@users.noreply.github.com>
2023-12-01 22:57:31 -0600

    ready for day 2

--- 01.js
+++ 01.js
@@ -6,31 +6,42 @@ const part1 = (data) => {
   return getTotal(arrayOfNumbers);
 };
 
+const processLines = (lines) => {
+  const arrayOfCalibrationValues = lines.map((line) => {
+    // replace non-digits with emtpy strings, leaving only numbers behind
+    const numbersOnly = line.replace(/\D/g, "");
+    // get the first number in the remaining string
+    const firstNumber = numbersOnly[0];
+    // get the last number of the remaining string
+    const lastNumber = numbersOnly[numbersOnly.length - 1];
+    // create a 2 digit number
+    return parseInt(`${firstNumber}${lastNumber}`);
+  });
+  return arrayOfCalibrationValues;
+};
+
+const getTotal = (arrayOfNumbers) => {
+  return arrayOfNumbers.reduce(
+    (partialSum, currentTotal) => partialSum + currentTotal,
+    0
+  );
+};
+
 const part2 = (data) => {
   const lines = helpers.splitByNewLine(data);
   const arrayOfNumbers = processLinesPart2(lines);
   return getTotal(arrayOfNumbers);
 };
 
-const processLines = (lines) => {
-  const arrayOfCalibrationValues = lines.map((line) => {
-    // replace non-digits with emtpy strings, leaving only numbers behind
-    const numbersOnly = line.replace(/\D/g, "");
-    const firstNumber = numbersOnly[0];
-    const lastNumber = numbersOnly[numbersOnly.length - 1];
-    return parseInt(`${firstNumber}${lastNumber}`);
-  });
-  return arrayOfCalibrationValues;
-};
-
 const processLinesPart2 = (lines) => {
-  const matchValues = lines.map((line) => {
+  // find all the numbers in the line (including strings)
+  const valueArray = lines.map((line) => {
     const matchedStrings = getNumberValues(line);
     return parseInt(
       `${matchedStrings[0]}${matchedStrings[matchedStrings.length - 1]}`
     );
   });
-  return matchValues;
+  return valueArray;
 };
 
 const getNumberValues = (string) => {
@@ -48,25 +59,20 @@ const getNumberValues = (string) => {
 
   return [
     ...string.matchAll(
+      // match all with a lookback to ensure overlaps are included
       new RegExp(
         /(?<=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))/,
         "g"
       )
     ),
   ].map((found) => {
+    // value is in group 1 of the matchAll
     const value = found[1];
     if (value.length > 1) {
       return valueLookup[value];
     }
     return value;
   });
-};
-
-const getTotal = (arrayOfNumbers) => {
-  return arrayOfNumbers.reduce(
-    (partialSum, currentTotal) => partialSum + currentTotal,
-    0
-  );
 };
 
 module.exports = {
--- helpers.js
+++ helpers.js
@@ -6,9 +6,17 @@ const splitByBlankLine = (data) => data.split(/\n\n/);
 
 const isUpperCase = (string) => /^[A-Z]*$/.test(string);
 
+const getTotal = (arrayOfNumbers) => {
+  return arrayOfNumbers.reduce(
+    (partialSum, currentTotal) => partialSum + currentTotal,
+    0
+  );
+};
+
 module.exports = {
   splitByNewLine,
   splitBySpace,
   splitByBlankLine,
   isUpperCase,
+  getTotal,
 };
