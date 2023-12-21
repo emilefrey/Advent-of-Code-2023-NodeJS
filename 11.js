268571284b4cde9d450544af80c4613561ea8933
Emile Frey <24356622+emilefrey@users.noreply.github.com>
2023-12-20 22:45:48 -0600

    day 11 complete

--- 11.js
+++ 11.js
@@ -1,42 +1,27 @@
 var helpers = require("./helpers");
 
 const detectAndAddRows = (data) => {
-  const updatedArray = [...data];
-  let rowsAdded = 0;
+  let rowsAdded = [];
   for (i = 0; i < data.length; i++) {
     if (!data[i].includes("#")) {
-      updatedArray.splice(i + rowsAdded, 0, ".".repeat(data[0].length));
-      rowsAdded++;
+      rowsAdded.push(i);
     }
   }
-  return updatedArray;
+  return rowsAdded;
 };
 
 const detectAndAddColumns = (data) => {
-  let updatedArray = [...data];
-  let columnsAdded = 0;
+  let columnsAdded = [];
   for (i = 0; i < data[0].length; i++) {
     for (j = 0; j < data.length; j++) {
-      // console.log(i, j, data[i][j]);
       if (data[j][i] === "#") {
-        // console.log(j, i);
         break;
       } else if (j === data.length - 1) {
-        updatedArray = updatedArray.map((line) => {
-          return (
-            line.slice(0, columnsAdded + i) + "." + line.slice(columnsAdded + i)
-          );
-        });
-        columnsAdded++;
+        columnsAdded.push(i);
       }
     }
-
-    // if (!data[i].includes("#")) {
-    //   updatedArray.splice(i + rowsAdded, 0, ".".repeat(data[0].length));
-    //   rowsAdded++;
-    // }
   }
-  return updatedArray;
+  return columnsAdded;
 };
 
 function getAllIndexes(arr, val) {
@@ -46,10 +31,10 @@ function getAllIndexes(arr, val) {
   return indexes;
 }
 
-const part1 = (data) => {
+const part1 = (data, expansionValue = 2) => {
   let lines = helpers.splitByNewLine(data);
-  lines = detectAndAddRows(lines);
-  lines = detectAndAddColumns(lines);
+  rowsAdded = detectAndAddRows(lines);
+  columnsAdded = detectAndAddColumns(lines);
   linesArray = lines.map((line) => line.split(""));
   const galaxies = [];
 
@@ -62,20 +47,31 @@ const part1 = (data) => {
 
   total = 0;
   for (let i = 0; i < galaxies.length; i++) {
-    console.log("inside", galaxies[i]);
     for (let j = i + 1; j < galaxies.length; j++) {
-      console.log("j loop");
+      const rowsCrossedWithExpansion = rowsAdded.filter(
+        (row) =>
+          (galaxies[i].line < row && row < galaxies[j].line) ||
+          (galaxies[j].line < row && row < galaxies[i].line)
+      );
+
+      const columnsCrossedWithExpansion = columnsAdded.filter(
+        (column) =>
+          (galaxies[i].column < column && column < galaxies[j].column) ||
+          (galaxies[j].column < column && column < galaxies[i].column)
+      );
+
       total +=
         Math.abs(galaxies[i].line - galaxies[j].line) +
-        Math.abs(galaxies[i].column - galaxies[j].column);
+        Math.abs(galaxies[i].column - galaxies[j].column) +
+        (rowsCrossedWithExpansion.length + columnsCrossedWithExpansion.length) *
+          (expansionValue - 1);
     }
   }
   return total;
 };
 
 const part2 = (data) => {
-  const lines = helpers.splitByNewLine(data);
-  return 2;
+  return part1(data, 1000000);
 };
 
 module.exports = {
