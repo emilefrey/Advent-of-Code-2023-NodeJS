e2cb9d6bb6f60213bd7ce86c7bd9c6cf86de0535
Emile Frey <24356622+emilefrey@users.noreply.github.com>
2023-12-20 22:47:19 -0600

    day 18 started

--- 18.js
+++ 18.js
@@ -2,8 +2,95 @@ var helpers = require("./helpers");
 
 const part1 = (data) => {
   const lines = helpers.splitByNewLine(data);
-  return 1;
+
+  const map = [[]];
+  let currentRow = 0;
+  let currentColumn = 0;
+
+  lines.forEach((line) => {
+    let [direction, amount] = line.split(" ");
+    amount = parseInt(amount);
+    if (direction === "U") {
+      const temp = currentRow;
+      while (currentRow > temp - amount) {
+        if (!map[currentRow]) {
+          map[currentRow] = [];
+        }
+        map[currentRow][currentColumn] = 1;
+        currentRow--;
+      }
+    } else if (direction === "D") {
+      const temp = currentRow;
+      while (currentRow < temp + amount) {
+        if (!map[currentRow]) {
+          map[currentRow] = [];
+        }
+        map[currentRow][currentColumn] = 1;
+        currentRow++;
+      }
+    } else if (direction === "R") {
+      const temp = currentColumn;
+      while (currentColumn < temp + amount) {
+        if (!map[currentRow]) {
+          map[currentRow] = [];
+        }
+        map[currentRow][currentColumn] = 1;
+
+        currentColumn++;
+      }
+    } else if (direction === "L") {
+      const temp = currentColumn;
+      while (currentColumn > temp - amount) {
+        if (!map[currentRow]) {
+          map[currentRow] = [];
+        }
+        map[currentRow][currentColumn] = 1;
+        currentColumn--;
+      }
+    }
+  });
+  console.table(map);
+
+  return map.reduce((accum, currentValue) => {
+    const temp = [];
+    let i = 0;
+    let start = false;
+    let inside = false;
+    let count = 0;
+    let hitGap = false;
+    while (i < currentValue.length) {
+      // if you hit a 1, you def count
+      if (currentValue[i]) {
+        count++;
+        i++;
+        if (!start) {
+          start = true;
+        }
+        continue;
+      } else if (!currentValue[i] && start) {
+        count++;
+        i++;
+        start = false;
+        inside = true;
+        continue;
+      } else if (!currentValue[i] && !start && inside) {
+        count++;
+        i++;
+        continue;
+      } else if (inside) {
+        count++;
+        i++;
+        continue;
+      }
+      i++;
+    }
+    console.log(count);
+    // const rowValue = currentValue.length - i;
+    return accum + count;
+  }, 0);
 };
+
+// 21438 too low
 
 const part2 = (data) => {
   const lines = helpers.splitByNewLine(data);
